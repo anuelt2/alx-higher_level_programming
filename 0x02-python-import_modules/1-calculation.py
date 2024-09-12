3de1b2b176d0e2bc56c90a39de68be39ce7bc107
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-12 12:23:01 +0000

    Add function calls before print function in 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,10 @@ import calculator_1
 if __name__ == "__main__":
     a = 10
     b = 5
+    calculator_1.add(a, b)
+    calculator_1.sub(a, b)
+    calculator_1.mul(a, b)
+    calculator_1.div(a, b)
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
