a5b025f11a719f4848b5965f866a57f06b05dcf4
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-12 20:34:29 +0000

    Modify 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-from sys import exit, argv
+from sys import argv, exit
 if __name__ == "__main__":
-    num_args = len(argv)
-    print(argv[3])
-    if num_args != 4:
+    num_of_args = len(argv)
+    if num_of_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
@@ -19,5 +18,5 @@ if __name__ == "__main__":
     elif operator == '/':
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, _, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
