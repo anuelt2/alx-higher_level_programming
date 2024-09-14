2df79bfb396cd61030f2fed3881370130db2f707
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-14 11:14:45 +0000

    Add format specifier to print statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     my_list.reverse()
     for i in range(length):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
