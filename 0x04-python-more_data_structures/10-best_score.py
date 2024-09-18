f546af75caacbe4a03e93e845a013474a479c30b
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-18 16:33:36 +0000

    Modify dictionary value existence condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         value_sort = sorted(a_dictionary.items(), key=lambda kv_pairs:
                             kv_pairs[1])
         return value_sort[-1][0]
