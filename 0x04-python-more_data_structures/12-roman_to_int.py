92e5834a1e42866347c9fe6cc3382200adc7c15a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-09-18 20:32:26 +0000

    Modify roman_to_int function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) is False or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     rom_num = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     int_num = [1, 5, 10, 50, 100, 500, 1000]
@@ -10,10 +10,8 @@ def roman_to_int(roman_string):
     number = 0
     for i in range(length - 1):
         if char_to_num[i] < char_to_num[i + 1]:
-            number += char_to_num[i + 1] - char_to_num[i]
-            i = i + 1
+            number -= char_to_num[i]
         else:
             number += char_to_num[i]
-    if char_to_num[length - 1] <= char_to_num[length - 2]:
-        number += char_to_num[length - 1]
+    number += char_to_num[length - 1]
     return number
