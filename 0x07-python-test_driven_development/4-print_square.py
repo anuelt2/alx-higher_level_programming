8ef8e7c577cca9c6848f98d8034e5c84e48bf7fe
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-06 00:05:57 +0000

    Add 4-print_sqaure.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module for print_square function."""
+
+
+def print_square(size):
+    """A function that prints a square with the # character.
+
+    Args:
+        size: Length of the square.
+
+    Raises:
+        TypeError: If size is not int.
+        ValueError: If size is less than 0.
+        TypeError: If size is a float and is less than 0.
+    """
+
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float):
+        if size < 0:
+            raise TypeError("size must be an integer")
+        size = int(size)
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    i = 0
+    while i < size:
+        for p in range(size):
+            print("#", end="")
+        print()
+        i += 1
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
