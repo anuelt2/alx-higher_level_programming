573e77a28e3e5eb8f3a4b307f78ef57fe354e35a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-14 17:02:44 +0000

    Add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a basegeometry class"""
+
+
+class BaseGeometry:
+    """A class representing a base geometry"""
+
+    def area(self):
+        """Unimplemented public instance method"""
+        raise Exception("area() is not implemented")
