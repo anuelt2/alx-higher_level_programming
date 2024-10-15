c7b0cf386c55bba79fa296a8bca061bce46f7d05
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-15 19:24:05 +0000

    Add 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a student class"""
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student, with first_name, last_name, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if (isinstance(attrs, list)
+                and all(isinstance(attr, str) for attr in attrs)):
+            attr_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    attr_dict[key] = value
+            return attr_dict
+        return self.__dict__
