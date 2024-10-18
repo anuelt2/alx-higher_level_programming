34940b463d15c6d75fe2fd3e84e454fd71c02a1e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-18 15:22:58 +0000

    Update class Square with public getter and setter methods for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,22 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    """size private instance attribute."""
+    @property
+    def size(self):
+        """Getter for size attribute."""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """Setter for size attribute."""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
+
     def __str__(self):
         """Custom __str__ method. Returns string representation of Square."""
         return ("[Square] (" + str(self.id) + ") " + str(self.x) + "/"
