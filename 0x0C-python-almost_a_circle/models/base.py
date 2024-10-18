e9262ac7181c4cde96a4cc9ea4a60a1917608922
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-18 19:13:43 +0000

    Update class Base with create class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,10 @@ class Base:
         if json_string is None or not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        dummy_instance = cls(1, 1, 1, 1, 1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
