e04df9c9be6139df8c122b695fbf5ba3b4960f99
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-11-20 20:53:09 +0000

    Modify 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,6 @@
 -- found in hbtn_0d_usa database
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+WHERE state_id = 
+(SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id;
