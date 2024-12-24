56a61aeabd87ebf579b0e0d1bc3156c19a9b0d80
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-12-24 02:41:59 +0000

    Modify LIKE operator in 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
             port=3306
             )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY "
+                "'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
