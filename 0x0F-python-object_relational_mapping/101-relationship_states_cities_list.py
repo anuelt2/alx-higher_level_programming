96082b7f149ef88554d289a1c6c393ea1fe79954
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-12-27 08:10:46 +0000

    Modify 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,7 +5,7 @@ City objects contained in a database
 """
 from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
@@ -19,12 +19,13 @@ if __name__ == "__main__":
             f'mysql+mysqldb://{username}:{password}@localhost/{db_name}',
             pool_pre_ping=True
             )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).options(joinedload(State.cities))\
-        .order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
     for state in states:
         print(f"{state.id}: {state.name}")
         for city in sorted(state.cities, key=lambda city: city.id):
             print(f"\t{city.id}: {city.name}")
+    session.close()
