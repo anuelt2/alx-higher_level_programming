714a258b9e710bfb5c4accd73d2cb7e20e4a93a0
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-16 15:29:01 +0000

    Remove OPTIONS HTTP mthod 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes URL and displays all HTTP methods the server will accept
-curl -isX OPTIONS "$1" | grep -i Allow | awk '{$1=""; sub(/^ /, ""); print $0}'
+curl -sI "$1" | grep -i Allow | awk '{$1=""; sub(/^ /, ""); print $0}'
