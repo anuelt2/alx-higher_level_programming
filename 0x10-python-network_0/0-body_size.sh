b62a489c917c74e46532adf6de8578e24001fe56
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-16 13:18:15 +0000

    Correct 0-body_size.sh to display value only without name

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes and sends request to URL, displays size of body of response
-curl -sI "$1" | grep -i Content-Length
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
