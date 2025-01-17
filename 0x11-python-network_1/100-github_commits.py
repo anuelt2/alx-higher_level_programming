da33dd205b476c4a63d49f81801bb158815027e7
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-17 20:15:30 +0000

    Modify 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,8 +10,12 @@ import requests
 if __name__ == "__main__":
     url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     response = requests.get(url)
-    json_data = response.json()
-    for commit in json_data[:10]:
-        sha = commit.get("sha")
-        author_name = commit.get("commit").get("author").get("name")
-        print(f"{sha}: {author_name}")
+    if response.status_code == 200:
+        json_data = response.json()
+        for commit in json_data[:10]:
+            sha = commit.get("sha")
+            author_name = (commit.get("commit", {})
+                           .get("author", {}).get("name", {}))
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"Error: {response.status_code}")
