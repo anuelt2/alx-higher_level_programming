a44b3897b5f6be06fb171bd9c2807cd814cf8a95
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-17 18:24:28 +0000

    Add 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that takes 2 arguments and lists 10 commits
+from the latest, of a GitHub repository
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    response = requests.get(url)
+    json_data = response.json()
+    for commit in json_data[:10]:
+        sha = commit.get("sha")
+        author_name = commit.get("commit").get("author").get("name")
+        print(f"{sha}: {author_name}")
