2badc35a1d8fdff57153fe664f5d00a2e6f43beb
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-11 14:30:32 +0000

    Remove conversion to json object in 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, { json: true }, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   }
