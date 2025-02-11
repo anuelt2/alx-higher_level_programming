91927e268272eed516423f532e51753071ade97c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-11 18:49:44 +0000

    Add 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  body.characters.forEach(character => {
+    request(character, { json: true }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+      console.log(body.name);
+    });
+  });
+});
