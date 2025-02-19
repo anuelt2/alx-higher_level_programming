5f4ca47ee9cf89fa5377087d1c2fa5bf46398da5
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-19 20:51:53 +0000

    Add 102-script.js

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+/* global $ */
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const langCode = $('INPUT#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/?lang=${langCode}`, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
